0f374cf0b0d407dada2387c74af30831d1c7a037
hoqradwan <92720128+hoqradwan@users.noreply.github.com>
2022-08-27 06:56:56 +0600

    navbar change

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -2,11 +2,25 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="ui fixed menu">
+   /*  <div className="ui fixed menu">
       <div className="ui container center">
         <h2>ClothesNGadget</h2>
       </div>
-    </div>
+    </div> */
+    <div style={{
+      backgroundColor: "#33343B",
+      height: "60px",
+      marginTop: "0" ,
+      color: "#D2D3D5",
+      textAlign: "center",
+      fontSize: "30px",
+      fontWeight: "semi-bold",
+      paddingTop: "18px"
+     
+
+   }}>
+      Menz Gallery & Pro
+   </div>
   );
 };
 
